05f2b48e8ad586ca23ce00b4ad0f46938eb04281
derek-d-wang <106024326+derek-d-wang@users.noreply.github.com>
2022-05-23 13:26:03 -0600

    Pt 1 of the tutorials
    Leaned about tags and HTML dividers

--- beautsoup.py
+++ beautsoup.py
@@ -2,3 +2,20 @@ import bs4 as bs
 import urllib.request
 
 source = urllib.request.urlopen('https://pythonprogramming.net/parsememcparseface').read()
+
+soup = bs.BeautifulSoup(source, 'lxml')
+
+print(soup)
+#print(soup.title)          # prints everything enclosed in <title> brackets
+#print(soup.title.string)   # prints
+
+print(soup.p)               # prints first paragraph
+#print(soup.find_all('p'))   # finds all the paragraph tags to print
+
+for paragraph in soup.find_all('p'):
+    print(paragraph.text)       # string throws none if there are child tags, text just prints the plaintext
+
+print(soup.get_text())      # prints all plaintext
+
+for url in soup.find_all('a'):  # 'a' tag is for links/urls
+    print(url.get('href'))
